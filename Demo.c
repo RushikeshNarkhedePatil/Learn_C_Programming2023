10a9c409c5eee19f4bb424673784abe6124b8661
RushikeshNarkhedePatil <rushikeshnarkhede4@gmail.com>
2023-07-30 14:59:34 +0530

    first commit

---
+++ Demo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    // Bytes to Allocate     1 Byte.
+    char ch ='A';   // ch is a variable of type character currently initilized with value A.
+    // Bytes to Allocate     4 Byte
+    int Number=105445212;  // number is a variable of type integer currently initilized with value 10.
+    // Bytes to Allocate     4 Byte
+    float fNo = 3.14;         // fNo is a variable 
+
+    printf("Jay Ganesh\n");
+    return 0;
+}
+
+
+// gcc gnu compiler
+// compile code :- gcc Demo.c -o myexe
+// Run ./myexe
