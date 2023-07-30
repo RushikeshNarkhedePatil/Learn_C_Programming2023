8b18d301f5690d10ab9a971c735bda4095d29b9f
RushikeshNarkhedePatil <rushikeshnarkhede4@gmail.com>
2023-07-30 15:05:17 +0530

    update

--- Demo.c
+++ Demo.c
@@ -6,7 +6,9 @@ int main()
     // Bytes to Allocate     4 Byte
     int Number=105445212;  // number is a variable of type integer currently initilized with value 10.
     // Bytes to Allocate     4 Byte
-    float fNo = 3.14;         // fNo is a variable 
+    float fNo = 3.14;         // fNo is a variable of type float currently initilized with value 3.14;
+    // Bytes 8 
+    double dNo= 3.14;          // dNo is a variable of type double currently initilized with value 3.14.
 
     printf("Jay Ganesh\n");
     return 0;
